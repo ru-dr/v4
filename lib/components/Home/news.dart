5fc50f4e8bf2555fadac5b3016a2c07b37814654
ru-dr <rudr26@proton.me>
2024-02-09 09:06:26 +0530

    Remove unused import statement

--- lib/components/Home/news.dart
+++ lib/components/Home/news.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class News extends StatefulWidget {
   const News({Key? key}) : super(key: key);
