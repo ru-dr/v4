ad4e26556c74fb7b30d32a85b738d1e7335cf55d
ru-dr <rudr26@proton.me>
2024-02-23 10:54:36 +0530

    Add Flutter Markdown package for chat message formatting

--- lib/screens/Yatri/yatri.dart
+++ lib/screens/Yatri/yatri.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class Yatri extends StatefulWidget {
@@ -10,51 +11,57 @@ class Yatri extends StatefulWidget {
 
 class _YatriState extends State<Yatri> {
   final TextEditingController _controller = TextEditingController();
-  final List<String> _messages = [];
+  final List<ChatMessage> _messages = [];
   late final GenerativeModel _model;
   late final ChatSession _chat;
 
   @override
   void initState() {
     super.initState();
-    const apiKey = 'AIzaSyBH5NYRQ0AyKg43_BJ4FT-qUjq85ZSuXFc'; // Replace with your actual API key
+    const apiKey =
+        'AIzaSyBH5NYRQ0AyKg43_BJ4FT-qUjq85ZSuXFc'; // Replace with your actual API key
     _model = GenerativeModel(
       model: 'gemini-pro',
       apiKey: apiKey,
-      generationConfig: GenerationConfig(maxOutputTokens: 100),
+      generationConfig: GenerationConfig(maxOutputTokens: 500),
     );
     _chat = _model.startChat(history: [
-      Content.text('Hello, I have 2 dogs in my house.'),
-      Content.model([TextPart('Great to meet you. What would you like to know?')])
+      Content.text(
+          'Hi, I am Yatri, your travel assistant. How can I help you today?'),
+      Content.model([
+        TextPart(
+            "YatraZen's own AI yatri is Based on Gemini Pro & here to help you with your travel queries")
+      ])
     ]);
   }
 
-  Future<void> sendMessage(String text) async {
-    try {
-      var content = Content.text(text);
-      var response = await _chat.sendMessage(content);
-      setState(() {
-        _messages.add('User: $text');
-        _messages.add('Bot: ${response.text}');
-      });
-      _controller.clear();
-    } catch (error) {
-      // Handle error here, e.g., display an error message to the user
-      print('Error sending message: $error');
+  Future<void> sendMessage(String? text) async {
+    if (text != null && text.isNotEmpty) {
+      try {
+        var content = Content.text(text);
+        var response = await _chat.sendMessage(content);
+        setState(() {
+          _messages.add(ChatMessage(text: text, isUser: true));
+          _messages.add(ChatMessage(text: response.text ?? "", isUser: false));
+        });
+        _controller.clear();
+      } catch (error) {
+        // Handle error here, e.g., display an error message to the user
+        print('Error sending message: $error');
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Dark theme background color
       appBar: AppBar(
         title: Text(
           "Yatri AI",
-          style: Theme.of(context).textTheme.bodySmall, // Title text color
+          style: Theme.of(context).textTheme.bodySmall,
         ),
-        backgroundColor: Colors.black, // App bar background color
-        iconTheme: const IconThemeData(color: Colors.white), // Icon color
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
         centerTitle: true,
       ),
       body: SafeArea(
@@ -64,28 +71,27 @@ class _YatriState extends State<Yatri> {
               child: ListView.builder(
                 itemCount: _messages.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      _messages[index],
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white, // Message text color
-                      ),
-                    ),
-                  );
+                  return ChatBubble(message: _messages[index]);
                 },
               ),
             ),
             Row(
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    style: TextStyle(color: Colors.white), // Text field text color
-                    decoration: InputDecoration(
-                      hintText: 'Enter your message',
-                      hintStyle: TextStyle(color: Colors.grey), // Hint text color
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      controller: _controller,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your message',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
                 ),
@@ -100,4 +106,66 @@ class _YatriState extends State<Yatri> {
       ),
     );
   }
+}
+
+class ChatBubble extends StatelessWidget {
+  final ChatMessage message;
+
+  const ChatBubble({required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: message.isUser ? Colors.blue : Colors.green,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: message.isUser
+                ? MainAxisAlignment.end
+                : MainAxisAlignment.start,
+            children: [
+              if (!message.isUser) ...[
+                const CircleAvatar(
+                  backgroundImage: AssetImage('assets/bot_image.png'),
+                  radius: 20,
+                ),
+                const SizedBox(width: 8),
+              ],
+              Flexible(
+                child: message.isMarkdown
+                    ? MarkdownBody(
+                        data: message.text,
+                        styleSheet: MarkdownStyleSheet(
+                          p: const TextStyle(color: Colors.white),
+                        ),
+                      )
+                    : Text(
+                        message.text,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+              ),
+              if (message.isUser) ...[
+                const SizedBox(width: 8),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/user_image.png'),
+                  radius: 20,
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage {
+  final String text;
+  final bool isUser;
+  final bool isMarkdown;
+
+  const ChatMessage(
+      {required this.text, required this.isUser, this.isMarkdown = false});
 }
