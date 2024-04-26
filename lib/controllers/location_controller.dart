55d896dd307e367498de22e14c1bdbe34a365c40
Lazycoder26 <Rudra26_2004@outlook.com>
2024-04-26 22:43:24 +0530

    Refactor code for improved readability and consistency in various files

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -4,7 +4,10 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
 
-List<String> emergencyContactNumbers = ["100","108"]; // Global variable to store emergency contact numbers
+List<String> emergencyContactNumbers = [
+  "100",
+  "108"
+]; // Global variable to store emergency contact numbers
 
 class LocationController extends GetxController {
   Position? currentPosition;
